828b23915c739018be6483d351e82b8e4353deeb
Sandrac98 <sandrarossales98@gmail.com>
2023-03-02 15:47:05 +0000

    add and style to do list

--- assets/js/script.js
+++ assets/js/script.js
@@ -210,3 +210,34 @@ document.addEventListener("DOMContentLoaded", () => {
     };
   
   });
+
+
+
+
+
+// TO-DO LIST
+document.querySelector('#newTaskButton').onclick = function(){
+  if(document.querySelector('#newtask input').value.length == 0){
+      alert("Enter Task Name")
+  }
+
+  else{
+      document.querySelector('#tasks').innerHTML += `
+          <div class="task">
+              <span id="taskname">
+                  ${document.querySelector('#newtask input').value}
+              </span>
+              <button class="delete">
+                  <i class="far fa-trash-alt"></i>
+              </button>
+          </div>
+      `;
+
+      var current_tasks = document.querySelectorAll(".delete");
+      for(var i=0; i<current_tasks.length; i++){
+          current_tasks[i].onclick = function(){
+              this.parentNode.remove();
+          }
+      }
+  }
+}
